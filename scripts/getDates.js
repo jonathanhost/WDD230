ddcbac9d1c9af009769cd70f9ca7b0cc36a3937b
Jonathan de Almeida <47408354+jonathanhost@users.noreply.github.com>
2023-10-05 22:03:10 -0300

    form1

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,37 @@
 document.querySelector("#modify").innerHTML = `<p> Last Modification: ${document.lastModified}</p>`;
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
+
+
+const kp1 = document.querySelector("#password");
+const kp2 = document.querySelector("#passwordrept");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		kp2.style.backgroundColor = "#fff0f3";
+		kp2.value = "";
+		kp2.focus();
+	} else {
+		message.style.display = "none";
+		kp2.style.backgroundColor = "#fff";
+		kp2.style.color = "#000";
+	}
+}
