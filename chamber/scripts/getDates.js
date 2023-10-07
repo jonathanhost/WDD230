97155c9251f598cafcebc73fc6444bfe2bfc7ce5
Jonathan de Almeida <47408354+jonathanhost@users.noreply.github.com>
2023-10-07 04:52:10 -0300

    formnovo1

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,3 +1,10 @@
 document.querySelector("#modify").innerHTML = `<p> Last Modification: ${document.lastModified}</p>`;
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+var dateacess = new Date();
+
+var dateformat = dateacess.toLocaleString();
+
+document.getElementById("timestamp").value = dateformat;
