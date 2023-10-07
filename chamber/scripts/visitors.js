b77c47156608ea5c5fc7beec007bc506dcd3b60a
Jonathan de Almeida <47408354+jonathanhost@users.noreply.github.com>
2023-10-07 05:41:42 -0300

    fixedchamber

--- chamber/scripts/visitors.js
+++ chamber/scripts/visitors.js
@@ -3,32 +3,24 @@ const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 const data = Date.now();
 let lastVisit = Number(window.localStorage.getItem("lastVisit-ls")) || 0;
-let days = data - lastVisit ;
-console.log(days)
-if (numVisits !== 0) 
-{
-	visitsDisplay.textContent = numVisits;
-	if(days < 86400000)
-	{
-		visitsDisplay.textContent = "Back so soon! Awesome!";
-	}
-	else
-	{
-		if(days <= 172800000)
-		{
-			visitsDisplay.textContent = `You last visited 1 day ago.`;
-		}
-		else
-		{
-			let difference = days / 86400000;
-			visitsDisplay.textContent = `You last visited ${difference} days ago.`;
+const millisecondsInADay = 24 * 60 * 60 * 1000;
+const daysSinceLastVisit = Math.floor((data - lastVisit) / millisecondsInADay);
+console.log(daysSinceLastVisit)
 
-		}
+if (lastVisit){
+
+
+	if (daysSinceLastVisit === 0) {
+		visitsDisplay.textContent = 'Back so soon! Awesome!';
+	} else {
+		visitsDisplay.textContent = `You last visited ${daysSinceLastVisit} ${daysSinceLastVisit === 1 ? 'day' : 'days'} ago.`;
 	}
-} else 
-{
-	visitsDisplay.textContent = "Welcome! Let us know if you have any questions.";
-	localStorage.setItem("lastVisit-ls", data);
+
+
+} else {
+// This is the user's first visit
+visitsDisplay.textContent = 'Welcome! Let us know if you have any questions.';
+localStorage.setItem('lastVisit-ls', data);
 }
 
 
@@ -37,10 +29,6 @@ if (numVisits !== 0)
 
 
 
-console.log(data - lastVisit)
 
 
-numVisits++;
 
-localStorage.setItem("numVisits-ls", numVisits);
-localStorage.setItem("lastVisit-ls", data);
