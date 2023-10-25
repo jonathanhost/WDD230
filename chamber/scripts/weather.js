f675feb9972e8f0da134fd4a08a6c57a4356f6e6
Jonathan de Almeida <47408354+jonathanhost@users.noreply.github.com>
2023-10-24 21:14:13 -0300

    finished

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,34 @@
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const descript = document.querySelector('#descript'); 
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-27.59&lon=-48.52&appid=154755306c2db4e5311c3ced5b758d2c';
+
+async function apiFetch() {
+try {
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data)
+    displayResults(data)
+  } else {
+      throw Error(await response.text());
+  }
+} catch (error) {
+    console.log(error);
+}
+}
+function displayResults(data) {
+    let celcius = data.main.temp - 273.15 
+  currentTemp.innerHTML = `<p>${celcius.toFixed(2)}&deg;C - ${data.weather[0].description.toUpperCase(1)}</P>`;
+  image = document.createElement('img');
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  image.setAttribute('src', `${iconsrc}`);
+  image.setAttribute('alt', `${desc}`);
+  weatherIcon.appendChild(image);
+}
+apiFetch();
+
